9940fabc0205b10645282279f3b29cffe366f5da
Radouane.Rerhioui <radouane.rerhioui@nsi.be>
2025-10-15 21:44:34 +0200

    add checkAvailability action

--- ezpark-main/reservation-ctx/src/main/java/com/ezpark/io/reservation/domain/port/outbound/ParkingSpotAntiCorruptionService.java
+++ ezpark-main/reservation-ctx/src/main/java/com/ezpark/io/reservation/domain/port/outbound/ParkingSpotAntiCorruptionService.java
@@ -5,8 +5,12 @@ package com.ezpark.io.reservation.domain.port.outbound;
 import com.ezpark.io.shared.kernel.SpotId;
 import com.ezpark.io.reservation.domain.port.outbound.model.SpotDetailsView;
 
+import java.time.Instant;
+
 public interface ParkingSpotAntiCorruptionService {
     boolean isSpotAvailable(SpotId spotId);
     SpotDetailsView getSpotDetails(SpotId spotId);
     void reserveSpot(SpotId spotId, com.ezpark.io.shared.kernel.ReservationId reservationId);
+    boolean checkAvailability(SpotId spotId, Instant startTime, Instant endTime);
+
 }
