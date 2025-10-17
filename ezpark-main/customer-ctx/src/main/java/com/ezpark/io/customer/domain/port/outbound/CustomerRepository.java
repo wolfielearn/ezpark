aaacc41b51de1689316299066b5b6e01603b76b0
Radouane.Rerhioui <radouane.rerhioui@nsi.be>
2025-10-17 10:37:42 +0200

    remove @Repository from domain repositories

--- ezpark-main/customer-ctx/src/main/java/com/ezpark/io/customer/domain/port/outbound/CustomerRepository.java
+++ ezpark-main/customer-ctx/src/main/java/com/ezpark/io/customer/domain/port/outbound/CustomerRepository.java
@@ -3,6 +3,7 @@ package com.ezpark.io.customer.domain.port.outbound;
 
 import com.ezpark.io.customer.domain.model.Customer;
 import com.ezpark.io.shared.kernel.CustomerId;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
--- ezpark-main/reservation-ctx/src/main/java/com/ezpark/io/reservation/domain/port/outbound/ReservationRepository.java
+++ ezpark-main/reservation-ctx/src/main/java/com/ezpark/io/reservation/domain/port/outbound/ReservationRepository.java
@@ -2,19 +2,22 @@ package com.ezpark.io.reservation.domain.port.outbound;
 
 
 import com.ezpark.io.reservation.domain.model.Reservation;
+import com.ezpark.io.reservation.domain.model.ReservationStatus;
 import com.ezpark.io.shared.kernel.CustomerId;
 import com.ezpark.io.shared.kernel.ReservationId;
+import com.ezpark.io.shared.kernel.SpotId;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+
 public interface ReservationRepository {
     Reservation save(Reservation reservation);
     Optional<Reservation> findById(ReservationId reservationId);
-
+    List<Reservation> findByCustomerIdAndStatus(CustomerId customerId, ReservationStatus status);
+    Optional<Reservation> findActiveBySpotId(SpotId spotId);
     // Custom method NOT in JpaRepository
     List<Reservation> findByCustomerId(CustomerId customerId);
 
-    // Another custom method
-    List<Reservation> findActiveReservations();
 }
