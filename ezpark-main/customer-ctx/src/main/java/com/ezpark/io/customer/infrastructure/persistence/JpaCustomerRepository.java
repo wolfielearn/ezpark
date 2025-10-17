2e13253c4fee9985d38cb9ae32e975c2d0e34441
Radouane.Rerhioui <radouane.rerhioui@nsi.be>
2025-10-17 10:31:22 +0200

    add @Repository annotation to JPA repos

--- ezpark-main/customer-ctx/src/main/java/com/ezpark/io/customer/infrastructure/persistence/JpaCustomerRepository.java
+++ ezpark-main/customer-ctx/src/main/java/com/ezpark/io/customer/infrastructure/persistence/JpaCustomerRepository.java
@@ -4,7 +4,9 @@ import com.ezpark.io.customer.domain.model.Customer;
 import com.ezpark.io.customer.domain.port.outbound.CustomerRepository;
 import com.ezpark.io.shared.kernel.CustomerId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface JpaCustomerRepository extends JpaRepository<Customer, CustomerId>, CustomerRepository{
 
 }
--- ezpark-main/parking-ctx/src/main/java/com/ezpark/io/parking/infrastructure/persistence/JpaParkingRepository.java
+++ ezpark-main/parking-ctx/src/main/java/com/ezpark/io/parking/infrastructure/persistence/JpaParkingRepository.java
@@ -3,7 +3,9 @@ package com.ezpark.io.parking.infrastructure.persistence;
 import com.ezpark.io.parking.domain.model.ParkingSpot;
 import com.ezpark.io.parking.domain.port.outbound.ParkingSpotRepository;
 import com.ezpark.io.shared.kernel.SpotId;
+import org.jmolecules.ddd.annotation.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public  interface JpaParkingRepository extends JpaRepository<ParkingSpot, SpotId>, ParkingSpotRepository {
 }
