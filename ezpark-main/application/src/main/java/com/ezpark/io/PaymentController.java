ffcb08d8dabe12ee2ae9e99d8b433264e09e27c6
Radouane.Rerhioui <radouane.rerhioui@nsi.be>
2025-10-22 15:53:11 +0200

    add a Mock of external service used by our contexts

---
+++ ezpark-main/application/src/main/java/com/ezpark/io/PaymentController.java
@@ -0,0 +1,19 @@
+package com.ezpark.io;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping("/payment")
+public class PaymentController {
+
+    @GetMapping("{id}")
+    public Reciept  payment(@PathVariable("id") String id){
+        // return "Success" ;
+        return new Reciept("Payment successfull", BigDecimal.valueOf(25));
+    }
+}
---
+++ ezpark-main/application/src/main/java/com/ezpark/io/Reciept.java
@@ -0,0 +1,6 @@
+package com.ezpark.io;
+
+import java.math.BigDecimal;
+
+public record Reciept(String message, BigDecimal amount) {
+}
