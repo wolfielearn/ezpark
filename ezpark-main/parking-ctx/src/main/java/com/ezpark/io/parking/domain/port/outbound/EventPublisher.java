6f6ec4877c6c19e188c64ab7b24f73966727fc9b
Radouane.Rerhioui <radouane.rerhioui@nsi.be>
2025-10-23 13:26:25 +0200

    remove mocks for EventPublisher and move keep only shared EventPublisher

--- ezpark-main/parking-ctx/src/main/java/com/ezpark/io/parking/domain/port/outbound/EventPublisher.java
+++
@@ -1,9 +0,0 @@
-package com.ezpark.io.parking.domain.port.outbound;
-
-
-import com.ezpark.io.shared.event.DomainEvent;
-
-public interface EventPublisher {
-    void publish(DomainEvent event);
-    void publish(DomainEvent... events);
-}
