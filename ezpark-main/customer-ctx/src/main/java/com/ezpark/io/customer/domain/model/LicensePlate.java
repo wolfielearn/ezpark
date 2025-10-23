78c3f22f20d41509ab876688d99cc2e50282850b
Radouane.Rerhioui <radouane.rerhioui@nsi.be>
2025-10-23 17:09:00 +0200

    add method  fromString() to LicensePlate

--- ezpark-main/customer-ctx/src/main/java/com/ezpark/io/customer/domain/model/LicensePlate.java
+++ ezpark-main/customer-ctx/src/main/java/com/ezpark/io/customer/domain/model/LicensePlate.java
@@ -1,9 +1,17 @@
 package com.ezpark.io.customer.domain.model;
 
+import com.ezpark.io.shared.kernel.CustomerId;
+
+import java.util.UUID;
+
 public record LicensePlate(String value) {
     public LicensePlate {
         if (value == null || value.trim().isEmpty()) {
             throw new IllegalArgumentException("License plate cannot be empty");
         }
     }
+    public static LicensePlate fromString(String value) {
+        return new LicensePlate(value);
+    }
+
 }
