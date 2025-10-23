4577959cc23994d5c0f9d576afc3d0af60469e74
Radouane.Rerhioui <radouane.rerhioui@nsi.be>
2025-10-23 16:55:35 +0200

    move Config to Shared and remove unnecessary classes

--- ezpark-main/shared/src/main/java/com/ezpark/io/shared/infrastructure/Config.java
+++ ezpark-main/shared/src/main/java/com/ezpark/io/shared/infrastructure/Config.java
@@ -1,4 +1,4 @@
-package com.ezpark.io.customer.infrastructure.config;
+package com.ezpark.io.shared.infrastructure;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
