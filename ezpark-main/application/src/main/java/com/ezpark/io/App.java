18871b8b4b41b9b83fb3b8090af8925b42473e77
Radouane.Rerhioui <radouane.rerhioui@nsi.be>
2025-10-24 16:51:16 +0200

    enable kafka in application and add properties for kafka and custom ObjectMapper config

--- ezpark-main/application/src/main/java/com/ezpark/io/App.java
+++ ezpark-main/application/src/main/java/com/ezpark/io/App.java
@@ -5,12 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.ezpark.io")
 @EntityScan(basePackages = "com.ezpark.io")
-
-public class App 
+@EnableKafka // 👈 MAKE SURE THIS IS PRESENT
+public class App
 {
     public static void main(String[] args) { SpringApplication.run(App.class, args);}
 }
--- ezpark-main/shared/src/main/java/com/ezpark/io/shared/infrastructure/Config.java
+++ ezpark-main/shared/src/main/java/com/ezpark/io/shared/infrastructure/Config.java
@@ -1,7 +1,11 @@
 package com.ezpark.io.shared.infrastructure;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -11,4 +15,14 @@ public class Config {
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
+
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+            return mapper;
+    }
 }
+
