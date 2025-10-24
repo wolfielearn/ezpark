06595ea8d92b96523aed2ee576fc6c5b16de19ad
Radouane.Rerhioui <radouane.rerhioui@nsi.be>
2025-10-24 09:34:45 +0200

    add data.sql to populate db with data and update yaml properties

---
+++ ezpark-main/application/src/main/resources/data.sql
@@ -0,0 +1,46 @@
+-- #################################################
+--Populate the parking table with data
+INSERT INTO parking.parking(spot_id, type, status, zone, floor, number, current_reservation_id)
+VALUES ('1', 'STANDARD',  'AVAILABLE', 'Zone-A', '1' , '01', null);
+
+INSERT INTO parking.parking(spot_id, type, status, zone, floor, number, current_reservation_id)
+VALUES ('2', 'STANDARD',  'AVAILABLE', 'Zone-A', '1' , '01', null);
+
+INSERT INTO parking.parking(spot_id, type, status, zone, floor, number, current_reservation_id)
+VALUES ('3', 'STANDARD',  'AVAILABLE', 'Zone-A', '1' , '01', null);
+
+INSERT INTO parking.parking(spot_id, type, status, zone, floor, number, current_reservation_id)
+VALUES ('4', 'STANDARD',  'AVAILABLE', 'Zone-A', '1' , '01', null);
+
+INSERT INTO parking.parking(spot_id, type, status, zone, floor, number, current_reservation_id)
+VALUES ('5', 'STANDARD',  'AVAILABLE', 'Zone-A', '1' , '02', null);
+
+INSERT INTO parking.parking(spot_id, type, status, zone, floor, number, current_reservation_id)
+VALUES ('6', 'STANDARD',  'AVAILABLE', 'Zone-A', '1' , '03', null);
+
+INSERT INTO parking.parking(spot_id, type, status, zone, floor, number, current_reservation_id)
+VALUES ('7', 'STANDARD',  'AVAILABLE', 'Zone-A', '1' , '04', null);
+
+INSERT INTO parking.parking(spot_id, type, status, zone, floor, number, current_reservation_id)
+VALUES ('8', 'STANDARD',  'AVAILABLE', 'Zone-A', '2' , '01', null);
+
+INSERT INTO parking.parking(spot_id, type, status, zone, floor, number, current_reservation_id)
+VALUES ('9', 'STANDARD',  'AVAILABLE', 'Zone-A', '2' , '03', null);
+
+INSERT INTO parking.parking(spot_id, type, status, zone, floor, number, current_reservation_id)
+VALUES ('10', 'STANDARD',  'AVAILABLE', 'Zone-B', '1' , '01', null);
+
+INSERT INTO parking.parking(spot_id, type, status, zone, floor, number, current_reservation_id)
+VALUES ('11', 'STANDARD',  'AVAILABLE', 'Zone-B', '1' , '02', null);
+
+INSERT INTO parking.parking(spot_id, type, status, zone, floor, number, current_reservation_id)
+VALUES ('12', 'STANDARD',  'AVAILABLE', 'Zone-B', '2' , '01', null);
+
+INSERT INTO parking.parking(spot_id, type, status, zone, floor, number, current_reservation_id)
+VALUES ('13', 'STANDARD',  'AVAILABLE', 'Zone-B', '2' , '02', null);
+
+INSERT INTO parking.parking(spot_id, type, status, zone, floor, number, current_reservation_id)
+VALUES ('14', 'STANDARD',  'AVAILABLE', 'Zone-B', '3' , '01', null);
+
+INSERT INTO parking.parking(spot_id, type, status, zone, floor, number, current_reservation_id)
+VALUES  ('15', 'STANDARD',  'AVAILABLE', 'Zone-B', '3' , '02', null);
