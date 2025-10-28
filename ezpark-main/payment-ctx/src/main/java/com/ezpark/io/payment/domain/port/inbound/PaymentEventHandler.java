691bb74e6dce36be5074e8d601f96737c7090bd3
Radouane.Rerhioui <radouane.rerhioui@nsi.be>
2025-10-28 10:20:44 +0100

    payment : add method handle PaymentAuthorizedRequest

--- ezpark-main/payment-ctx/src/main/java/com/ezpark/io/payment/domain/port/inbound/PaymentEventHandler.java
+++ ezpark-main/payment-ctx/src/main/java/com/ezpark/io/payment/domain/port/inbound/PaymentEventHandler.java
@@ -3,8 +3,8 @@ package com.ezpark.io.payment.domain.port.inbound;
 import com.ezpark.io.shared.event.PaymentAuthorizationRequestedEvent;
 
 public interface PaymentEventHandler {
-    void handleReservationRequested(PaymentAuthorizationRequestedEvent event);       // From Customer Context
-   // void handlePaymentAuthorizationRequested(PaymentAuthorizationRequestedEvent event); // From Reservation Context
+    void handlePaymentAuthorizationRequested(PaymentAuthorizationRequestedEvent event); // From Reservation Context
+    //void handleReservationRequested();       // From Customer Context
     //void handleCheckOutCompleted(CheckOutCompletedEvent event);             // From Parking Context
     //void handleReservationCancelled(ReservationCancelledEvent event);       // From Reservation Context
 
