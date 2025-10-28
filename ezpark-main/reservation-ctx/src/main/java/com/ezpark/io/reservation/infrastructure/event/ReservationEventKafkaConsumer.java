8a3111274f030f8e9241aab54ceb581d64003a2f
Radouane.Rerhioui <radouane.rerhioui@nsi.be>
2025-10-28 10:13:32 +0100

    reservation : add kafka consumer

---
+++ ezpark-main/reservation-ctx/src/main/java/com/ezpark/io/reservation/infrastructure/event/ReservationEventKafkaConsumer.java
@@ -0,0 +1,53 @@
+package com.ezpark.io.reservation.infrastructure.event;
+
+import com.ezpark.io.reservation.domain.port.inbound.ReservationEventHandler;
+import com.ezpark.io.shared.event.PaymentAuthorizedEvent;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ReservationEventKafkaConsumer {
+    Logger log = LoggerFactory.getLogger(ReservationEventKafkaConsumer.class);
+
+    private final ReservationEventHandler reservationEventHandler;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public ReservationEventKafkaConsumer(ReservationEventHandler reservationEventHandler) {
+        this.reservationEventHandler = reservationEventHandler;
+
+        // EXPLICITLY register the JavaTimeModule => to avoid problem in Instant conversion
+        this.objectMapper.registerModule(new JavaTimeModule());
+        this.objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        log.info("ObjectMapper configured with JavaTimeModule");
+    }
+
+    @KafkaListener(topics = "reservation-events", groupId = "payment-ctx-group")
+    public void handlePaymentAuthorized(String message) {
+        log.info("--------HANDLE PaymentAuthorizedEvent CALLED SUCCESSFULLY----------------");
+        try {
+            System.out.println("message :"+ message);
+
+
+            JsonNode jsonNode = objectMapper.readTree(message);
+            String eventType = jsonNode.get("eventType").asText();
+
+            if ("PaymentAuthorizedEvent".equals(eventType)){
+                PaymentAuthorizedEvent event = objectMapper.readValue(message, PaymentAuthorizedEvent.class);
+                log.info("Received PaymentAuthorizedEvent: {}", event.getEventId());
+                reservationEventHandler.handlePaymentAuthorized(event);
+            }else {
+                log.debug("Skipping non-PaymentAuthorized event: {}", eventType);
+
+            }
+        } catch (Exception e) {
+            log.error("Failed to process payment authorization message. Message: {}", message, e);
+        }
+    }
+
+}
