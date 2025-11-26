56fd32cbab9f23c9616c92a7a0afe2c90899e51c
Radouane.Rerhioui <radouane.rerhioui@nsi.be>
2025-11-26 13:18:25 +0100

    ezpark-main: remove annotation @SpringbootApplication

--- ezpark-main/src/main/java/com/ezpark/io/ezpark_main/EzparkMainApplication.java
+++ ezpark-main/src/main/java/com/ezpark/io/ezpark_main/EzparkMainApplication.java
@@ -3,7 +3,7 @@ package com.ezpark.io.ezpark_main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class EzparkMainApplication {
 
 	public static void main(String[] args) {
