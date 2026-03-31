e97c23d1c61decccfc2122452406bf7dc3349dbd
Radouane.Rerhioui <radouane.rerhioui@nsi.be>
2026-03-31 09:41:11 +0200

    update application yaml (add init mode to sql) and also update the data.sql to use UUID as primary key for spotId

--- ezpark-main/src/main/resources/data.sql
+++ ezpark-main/src/main/resources/data.sql
@@ -1,46 +1,48 @@
 -- #################################################
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 --Populate the parking table with data
 INSERT INTO parking.parking(spot_id, type, status, zone, floor, number, current_reservation_id)
-VALUES ('1', 'STANDARD',  'AVAILABLE', 'Zone-A', '1' , '01', null);
+VALUES (uuid_generate_v4(), 'STANDARD',  'AVAILABLE', 'Zone-A', '1' , '01', null);
 
 INSERT INTO parking.parking(spot_id, type, status, zone, floor, number, current_reservation_id)
-VALUES ('2', 'STANDARD',  'AVAILABLE', 'Zone-A', '1' , '01', null);
+VALUES (uuid_generate_v4(),  'STANDARD',  'AVAILABLE', 'Zone-A', '1' , '01', null);
 
 INSERT INTO parking.parking(spot_id, type, status, zone, floor, number, current_reservation_id)
-VALUES ('3', 'STANDARD',  'AVAILABLE', 'Zone-A', '1' , '01', null);
+VALUES (uuid_generate_v4(), 'STANDARD',  'AVAILABLE', 'Zone-A', '1' , '01', null);
 
 INSERT INTO parking.parking(spot_id, type, status, zone, floor, number, current_reservation_id)
-VALUES ('4', 'STANDARD',  'AVAILABLE', 'Zone-A', '1' , '01', null);
+VALUES (uuid_generate_v4(), 'STANDARD',  'AVAILABLE', 'Zone-A', '1' , '01', null);
 
 INSERT INTO parking.parking(spot_id, type, status, zone, floor, number, current_reservation_id)
-VALUES ('5', 'STANDARD',  'AVAILABLE', 'Zone-A', '1' , '02', null);
+VALUES (uuid_generate_v4(), 'STANDARD',  'AVAILABLE', 'Zone-A', '1' , '02', null);
 
 INSERT INTO parking.parking(spot_id, type, status, zone, floor, number, current_reservation_id)
-VALUES ('6', 'STANDARD',  'AVAILABLE', 'Zone-A', '1' , '03', null);
+VALUES (uuid_generate_v4(), 'STANDARD',  'AVAILABLE', 'Zone-A', '1' , '03', null);
 
 INSERT INTO parking.parking(spot_id, type, status, zone, floor, number, current_reservation_id)
-VALUES ('7', 'STANDARD',  'AVAILABLE', 'Zone-A', '1' , '04', null);
+VALUES (uuid_generate_v4(), 'STANDARD',  'AVAILABLE', 'Zone-A', '1' , '04', null);
 
 INSERT INTO parking.parking(spot_id, type, status, zone, floor, number, current_reservation_id)
-VALUES ('8', 'STANDARD',  'AVAILABLE', 'Zone-A', '2' , '01', null);
+VALUES (uuid_generate_v4(), 'STANDARD',  'AVAILABLE', 'Zone-A', '2' , '01', null);
 
 INSERT INTO parking.parking(spot_id, type, status, zone, floor, number, current_reservation_id)
-VALUES ('9', 'STANDARD',  'AVAILABLE', 'Zone-A', '2' , '03', null);
+VALUES (uuid_generate_v4() , 'STANDARD',  'AVAILABLE', 'Zone-A', '2' , '03', null);
 
 INSERT INTO parking.parking(spot_id, type, status, zone, floor, number, current_reservation_id)
-VALUES ('10', 'STANDARD',  'AVAILABLE', 'Zone-B', '1' , '01', null);
+VALUES (uuid_generate_v4(), 'STANDARD',  'AVAILABLE', 'Zone-B', '1' , '01', null);
 
 INSERT INTO parking.parking(spot_id, type, status, zone, floor, number, current_reservation_id)
-VALUES ('11', 'STANDARD',  'AVAILABLE', 'Zone-B', '1' , '02', null);
+VALUES (uuid_generate_v4(), 'STANDARD',  'AVAILABLE', 'Zone-B', '1' , '02', null);
 
 INSERT INTO parking.parking(spot_id, type, status, zone, floor, number, current_reservation_id)
-VALUES ('12', 'STANDARD',  'AVAILABLE', 'Zone-B', '2' , '01', null);
+VALUES (uuid_generate_v4(), 'STANDARD',  'AVAILABLE', 'Zone-B', '2' , '01', null);
 
 INSERT INTO parking.parking(spot_id, type, status, zone, floor, number, current_reservation_id)
-VALUES ('13', 'STANDARD',  'AVAILABLE', 'Zone-B', '2' , '02', null);
+VALUES (uuid_generate_v4(), 'STANDARD',  'AVAILABLE', 'Zone-B', '2' , '02', null);
 
 INSERT INTO parking.parking(spot_id, type, status, zone, floor, number, current_reservation_id)
-VALUES ('14', 'STANDARD',  'AVAILABLE', 'Zone-B', '3' , '01', null);
+VALUES (uuid_generate_v4(), 'STANDARD',  'AVAILABLE', 'Zone-B', '3' , '01', null);
 
 INSERT INTO parking.parking(spot_id, type, status, zone, floor, number, current_reservation_id)
-VALUES  ('15', 'STANDARD',  'AVAILABLE', 'Zone-B', '3' , '02', null);
+VALUES  (uuid_generate_v4() ,'STANDARD',  'AVAILABLE', 'Zone-B', '3' , '02', null);
