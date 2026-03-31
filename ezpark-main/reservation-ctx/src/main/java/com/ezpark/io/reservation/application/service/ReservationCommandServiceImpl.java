02ddd4afdce54056990f8f258039a3f40e45e413
Radouane.Rerhioui <radouane.rerhioui@nsi.be>
2026-03-31 10:12:53 +0200

    refactor the method createReservation: checkAvailability is now Synchronous, and publish events for other cases

--- ezpark-main/reservation-ctx/src/main/java/com/ezpark/io/reservation/application/service/ReservationCommandServiceImpl.java
+++ ezpark-main/reservation-ctx/src/main/java/com/ezpark/io/reservation/application/service/ReservationCommandServiceImpl.java
@@ -5,7 +5,7 @@ import com.ezpark.io.reservation.domain.model.Reservation;
 import com.ezpark.io.reservation.domain.model.TimeSlot;
 import com.ezpark.io.reservation.domain.port.inbound.ReservationCommandService;
 import com.ezpark.io.reservation.domain.port.outbound.CustomerAntiCorruptionService;
-import com.ezpark.io.reservation.domain.port.outbound.ParkingSpotAntiCorruptionService;
+import com.ezpark.io.reservation.domain.port.outbound.ParkingSpotACLService;
 import com.ezpark.io.reservation.domain.port.outbound.ReservationRepository;
 import com.ezpark.io.shared.event.*;
 import com.ezpark.io.shared.kernel.CustomerId;
@@ -15,7 +15,6 @@ import com.ezpark.io.shared.kernel.SpotId;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.math.BigDecimal;
 import java.time.Instant;
 
 @Service
@@ -24,12 +23,12 @@ public class ReservationCommandServiceImpl implements ReservationCommandService 
 
     private final ReservationRepository reservationRepository;
     private final CustomerAntiCorruptionService customerACL;
-    private final ParkingSpotAntiCorruptionService parkingACL;
+    private final ParkingSpotACLService parkingACL;
     private final EventPublisher eventPublisher;
 
     public ReservationCommandServiceImpl(ReservationRepository reservationRepository,
                                          CustomerAntiCorruptionService customerACL,
-                                         ParkingSpotAntiCorruptionService parkingACL,
+                                         ParkingSpotACLService parkingACL,
                                          EventPublisher eventPublisher) {
         this.reservationRepository = reservationRepository;
         this.customerACL = customerACL;
@@ -48,27 +47,28 @@ public class ReservationCommandServiceImpl implements ReservationCommandService 
         Instant startTime = timeSlot.startTime();
         Instant endTime = timeSlot.endTime();
         // 2. Check spot availability
+        // use of synchronous call, because it's a query that does not change state of the parking context
+        // we need to know immediately if the spot is available or not to proceed with reservation creation
+
         boolean isAvailable = parkingACL.checkAvailability(spotId.value(), startTime, endTime);
         if (!isAvailable) {
 //            eventPublisher.publish(new ReservationFailedEvent(
-//                    ReservationId.newId(), spotId, customerId, "Spot not available"
+//                     null, spotId, customerId, "Spot not available"
 //            ));
             throw new IllegalArgumentException("Spot not available for requested time");
         }
-
         // 3. Create reservation
         Reservation reservation = Reservation.create(customerId, spotId, timeSlot);
         Reservation savedReservation = reservationRepository.save(reservation);
 
-        // 4. Request payment authorization
+        // 4. Publish ReservationRequested
         eventPublisher.publish(new ReservationRequestedEvent(
-                customerId.value(),                 // UUID
-                savedReservation.getId().value(),    // UUID
-                spotId.value(),                     // String
+                customerId.value(),
+                savedReservation.getId().value(),
+                spotId.value(),
                 startTime,
                 endTime
         ));
-
         return savedReservation.getId();
     }
 
@@ -77,17 +77,16 @@ public class ReservationCommandServiceImpl implements ReservationCommandService 
 
         Reservation reservation = reservationRepository.findById(reservationId)
                 .orElseThrow(() -> new IllegalArgumentException("Reservation not found"));
+       // Reservation savedReservation =null;
        try{
             reservation.confirm(paymentAuthId);
             reservationRepository.save(reservation);
-            // Reserve the physical spot
-           // parkingACL.reserveSpot(reservation.getSpotId().value(), reservationId.value());
-
-            eventPublisher.publish(new ReservationConfirmedEvent(
-                    reservation.getId().value(),
-                    reservation.getPaymentAuthId().value(),
-                    reservation.getSpotId().value(),
-                    reservation.getCustomerId().value()));
+           // Reserve the physical spot
+           eventPublisher.publish(new ReservationConfirmedEvent(
+                   reservation.getId().value(),
+                   reservation.getPaymentAuthId().value(),
+                   reservation.getSpotId().value(),
+                   reservation.getCustomerId().value()));
        } catch (Exception ex){
            eventPublisher.publish(new ReservationFailedEvent(
                    reservation.getId(),
@@ -96,8 +95,6 @@ public class ReservationCommandServiceImpl implements ReservationCommandService 
                    ex.getMessage()));
            throw ex;
        }
-
-
     }
 
     @Override
@@ -107,12 +104,11 @@ public class ReservationCommandServiceImpl implements ReservationCommandService 
 
         reservation.cancel();
         reservationRepository.save(reservation);
-
-//        eventPublisher.publish(new ReservationCancelledEvent(
-//                reservationId.value(),
-//                reservation.getCustomerId().value(),
-//                "Customer cancelled"
-//        ));
+        eventPublisher.publish(new ReservationCancelledEvent(
+                reservationId.value(),
+                reservation.getCustomerId().value(),
+                "Reservation cancelled by customer"
+        ));
     }
 
     @Override
@@ -132,6 +128,6 @@ public class ReservationCommandServiceImpl implements ReservationCommandService 
         reservation.markAsCompleted();
         reservationRepository.save(reservation);
 
-//        eventPublisher.publish(new ReservationCompletedEvent(reservationId.value()));
+       eventPublisher.publish(new ReservationCompletedEvent(reservationId.value()));
     }
 }
