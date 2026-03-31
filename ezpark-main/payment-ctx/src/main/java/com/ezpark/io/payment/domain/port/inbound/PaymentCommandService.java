ebe3a32742bea42a581a30802747e5e8903a4b67
Radouane.Rerhioui <radouane.rerhioui@nsi.be>
2026-03-31 10:20:50 +0200

    add spotId to the method authorizePayment

--- ezpark-main/payment-ctx/src/main/java/com/ezpark/io/payment/domain/port/inbound/PaymentCommandService.java
+++ ezpark-main/payment-ctx/src/main/java/com/ezpark/io/payment/domain/port/inbound/PaymentCommandService.java
@@ -5,9 +5,10 @@ import com.ezpark.io.payment.domain.model.Amount;
 import com.ezpark.io.payment.domain.model.PaymentMethod;
 import com.ezpark.io.shared.kernel.PaymentAuthorizationId;
 import com.ezpark.io.shared.kernel.ReservationId;
+import com.ezpark.io.shared.kernel.SpotId;
 
 public interface PaymentCommandService {
-    PaymentAuthorizationId authorizePayment(ReservationId reservationId, Amount amount, PaymentMethod paymentMethod);
+    PaymentAuthorizationId authorizePayment(ReservationId reservationId, SpotId spotId,Amount amount, PaymentMethod paymentMethod);
     void capturePayment(PaymentAuthorizationId paymentAuthId);
     void expirePayment(PaymentAuthorizationId paymentAuthId);
     void refundPayment(PaymentAuthorizationId paymentAuthId);
