69f47b47297d1b8f085b98cefea2cdd1ca2807c0
Radouane.Rerhioui <radouane.rerhioui@nsi.be>
2026-03-31 10:16:49 +0200

    PaymentEventHandler is  now listening to ReservationRequested

--- ezpark-main/payment-ctx/src/main/java/com/ezpark/io/payment/domain/port/inbound/PaymentEventHandler.java
+++ ezpark-main/payment-ctx/src/main/java/com/ezpark/io/payment/domain/port/inbound/PaymentEventHandler.java
@@ -1,10 +1,9 @@
 package com.ezpark.io.payment.domain.port.inbound;
 
-import com.ezpark.io.shared.event.PaymentAuthorizationRequestedEvent;
+import com.ezpark.io.shared.event.ReservationRequestedEvent;
 
 public interface PaymentEventHandler {
-    void handlePaymentAuthorizationRequested(PaymentAuthorizationRequestedEvent event); // From Reservation Context
-    //void handleReservationRequested();       // From Customer Context
+    void handleReservationRequested(ReservationRequestedEvent event); // From Reservation Context
     //void handleCheckOutCompleted(CheckOutCompletedEvent event);             // From Parking Context
     //void handleReservationCancelled(ReservationCancelledEvent event);       // From Reservation Context
 
